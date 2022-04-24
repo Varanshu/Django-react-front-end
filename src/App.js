5437fa15da561c8a1e38cbe9259780ef8e578dba
unknown <30varanshu@gmail.com>
2022-04-24 19:12:59 +0530

    add react router and react router bootstrap and integrate

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./bootstrap.min.css";
 import { Container } from "react-bootstrap";
-
+import ProductScreen from "./screens/ProductScreen";
 import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen/>
+          <Routes>
+            <Route path="/" element={<HomeScreen/>} exact />
+            <Route path="/product/:id" element={<ProductScreen/>}/>
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
