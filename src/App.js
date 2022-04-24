1fd81f2f67948cf60044f8b8dcbef1b521a5246b
unknown <30varanshu@gmail.com>
2022-04-24 18:06:16 +0530

    starting setup of bootstrap, header and font awesome

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./bootstrap.min.css";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
